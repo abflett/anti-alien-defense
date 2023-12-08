f27bd53911cd56ddcac2a277b3169ae7f83beb9c
Adam Flett <adam@adamflett.net>
2023-12-08 15:49:33 +0800

    added player class that allows flipping sprite

---
+++ player.lua
@@ -0,0 +1,27 @@
+player={
+    x=8,
+    y=8,
+    flipx=false,
+    flipy=false,
+    sprite=1,
+    update=function()
+        if btn(⬅️) then
+            player.x+=-1
+            player.flipx=false
+            player.flipy=false
+        end
+        if btn(➡️) then
+            player.x+=1
+            player.flipx=true
+            player.flipy=false
+        end
+        if btn(⬆️) then
+            player.y+=-1
+            player.flipy=true
+        end
+        if btn(⬇️) then
+            player.y+=1
+            player.flipy=false
+        end
+    end
+}
