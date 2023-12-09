7d5210a79d51535c875b1aeb84925b4cdcd1444d
Adam Flett <adam@adamflett.net>
2023-12-09 12:02:35 +0800

    testing cursor

--- player.lua
+++ player.lua
@@ -1,27 +1,30 @@
 player={
-    x=8,
-    y=8,
+    x=63,
+    y=63,
     flipx=false,
     flipy=false,
     sprite=1,
+    delay=1,
+    lastdelay=0,
 
     update=function()
+        print("time " ..time())
         if btn(⬅️) then
-            player.x+=-1
+            player.x+=-8
             player.flipx=false
             player.flipy=false
         end
         if btn(➡️) then
-            player.x+=1
+            player.x+=8
             player.flipx=true
             player.flipy=false
         end
         if btn(⬆️) then
-            player.y+=-1
+            player.y+=-8
             player.flipy=true
         end
         if btn(⬇️) then
-            player.y+=1
+            player.y+=8
             player.flipy=false
         end
     end,
